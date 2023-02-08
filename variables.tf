8cf28c5a1744d303dbc593e015b04761fa872231
AkashSridhar1 <124769656+AkashSridhar1@users.noreply.github.com>
2023-02-09 01:00:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "us-east-2"
 }
+
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
   default     =  "mykey"
