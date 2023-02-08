e6ac7bf854edb49d8e820063ab36d3af892594a6
AkashSridhar1 <124769656+AkashSridhar1@users.noreply.github.com>
2023-02-09 00:50:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "us-east-2"
 }
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "Jenkins"
+}
 
 variable "instance_type" {
   description = "instance type for ec2"
